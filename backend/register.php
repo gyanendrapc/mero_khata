af95d7e651832bab3b1eaa3ea9d13734bc7e4e46
gyanendra chaudhary <chaudharygyane699@gmail.com>
2021-03-15 04:37:30 -0700

    second commit for setting up signup page with email verification

---
+++ backend/register.php
@@ -0,0 +1,70 @@
+<?php
+// starting session
+session_start();
+$ERROR=NULL;
+$_SESSION["ERROR"] = "";
+
+// define variables
+$userfullname = $useremail = $usercontact = $useraddress = $userpassword = "";
+
+// getting data
+if (isset($_POST['submit'])) {
+  if ($_SERVER['REQUEST_METHOD']=='POST') {
+    $userfullname = test_input($_POST["user-name"]);
+    $usercontact = test_input($_POST["user-contact"]);
+    $useraddress = test_input($_POST["user-address"]);
+    $useremail = test_input($_POST["user-email"]);
+    $userpassword = test_input($_POST["user-password"]);
+    $userpassword1 = test_input($_POST["user-confirm-password"]);
+    
+    
+  }
+}
+
+// verifying data
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+// echo $userfullname;
+
+// checking username length
+if(strlen($userfullname)<5){
+  $ERROR = "<p>Your user name must be at least 5 character</p>";
+}elseif($userpassword != $userpassword1){
+  $ERROR .= "<p>Your password do not match </p>";
+}else{
+  // form is valid
+  require_once 'db.php';
+  
+  $vkey = md5(time().$userfullname);
+  
+  // inserting into database
+  $userpassword = md5($userpassword);
+  
+  $sql = "insert into users (username, contact, address, email, password, vkey ) values ('`$userfullname`','`$usercontact`','`$useraddress`','`$useremail`','`$userpassword`','`$vkey`')";
+  
+  if (mysqli_query($conn, $sql)) {
+    // echo "New record created successfully";
+    $to = $useremail;
+    $subject = "Email verification";
+    $message = "Click here <a href='http://localhost/mero_khata/backend/veryfy.php?vkey=`$vkey`'> to register account</a>";
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";    
+    $headers .= 'From: chaudharygyane699@gmail.com' . "\r\n";
+    mail($to,$subject,$message,$headers);
+
+    echo $to;
+
+    // header("location: thankyou.php");
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+  
+  mysqli_close($conn);
+  
+}
+
+?>
--- signup.php
+++ signup.php
@@ -1,94 +1,111 @@
 <?php require './components/header.php'?>
+<?php
+$ERROR = NULL;
+if($ERROR){
+echo "<script>alert(".$ERROR.")";
+
+}
+?>
 
 <div id="frontpage" class="">
-<?php include './components/navbar1.php'?>
+  <?php include './components/navbar1.php'?>
 
-<div class="row h-100 mx-0">
-<div class="col-md-7">
-<div class="fontpage-site-title">
-<div class="content">
-<h1>Mero Khata</h1>
-<p>No more papers</p>
-</div>
-</div>
-</div>
-<div class="col-md-5">
-<div class="font-page-form">
-<form id="login-form" class="shadow p-3 mt-5">
-<div class="form-group m-0">
-<h3>Signup</h3>
-</div>
-<div class="form-group m-0">
-<label for="user-name">Full Name</label>
-<input
-type="text"
-class="form-control form-contraol-sm"
-id="user-name"
-name="user-name"
-aria-describedby="emailHelp"
-placeholder="Enter Your Name"
-/>
-</div>
-<div class="form-group m-0">
-<label for="user-contact">Contact</label>
-<input
-type="tel"
-class="form-control form-contraol-sm"
-id="user-contact"
-name="user-contact"
-placeholder="Contact Number"
-/>
-</div>
-<div class="form-group m-0">
-<label for="user-address">Address</label>
-<input
-type="text"
-class="form-control form-contraol-sm"
-id="user-address"
-name="user-address"
-placeholder="Your Address"
-/>
-</div>
-<div class="form-group m-0">
-<label for="user-email">Email</label>
-<input
-type="text"
-class="form-control form-contraol-sm"
-id="user-email"
-name="user-email"
-placeholder="Your Email"
-/>
-</div>
-<div class="form-group m-0">
-<label for="user-password">Password</label>
-<input
-type="text"
-class="form-control form-contraol-sm"
-id="user-password"
-name="user-password"
-placeholder="Enter Password"
-/>
-</div>
-<div class="form-group m-0">
-<label for="user-password">Password</label>
-<input
-type="text"
-class="form-control form-contraol-sm"
-id="user-password"
-name="user-password"
-placeholder="Enter Password"
-/>
-</div>
-<div class="form-group m-0 mt-2 d-flex justify-content-center">
-<button type="submit" class="btn">Signup</button>
-</div>
-</form>
-</div>
-</div>
-</div>
+  <div class="row h-100 mx-0">
+    <div class="col-md-7">
+      <div class="fontpage-site-title">
+        <div class="content">
+          <h1>Mero Khata</h1>
+          <p>No more papers</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-5">
+      <div class="font-page-form">
+        <form
+          method="POST"
+          action="./backend/register.php"
+          id="login-form"
+          class="shadow p-3 mt-5"
+        >
+          <div class="form-group m-0">
+            <h3>Signup</h3>
+          </div>
+          <div class="form-group m-0">
+            <label for="user-name">Full Name</label>
+            <input
+              type="text"
+              class="form-control form-contraol-sm"
+              id="user-name"
+              name="user-name"
+              aria-describedby="emailHelp"
+              placeholder="Enter Your Name"
+              required
+            />
+          </div>
+          <div class="form-group m-0">
+            <label for="user-contact">Contact</label>
+            <input
+              type="tel"
+              class="form-control form-contraol-sm"
+              id="user-contact"
+              name="user-contact"
+              placeholder="Contact Number"
+              required
+            />
+          </div>
+          <div class="form-group m-0">
+            <label for="user-address">Address</label>
+            <input
+              type="text"
+              class="form-control form-contraol-sm"
+              id="user-address"
+              name="user-address"
+              placeholder="Your Address"
+              required
+            />
+          </div>
+          <div class="form-group m-0">
+            <label for="user-email">Email</label>
+            <input
+              type="email"
+              class="form-control form-contraol-sm"
+              id="user-email"
+              name="user-email"
+              placeholder="Your Email"
+              required
+            />
+          </div>
+          <div class="form-group m-0">
+            <label for="user-password">Password</label>
+            <input
+              type="password"
+              class="form-control form-contraol-sm"
+              id="user-password"
+              name="user-password"
+              placeholder="Enter Password"
+              required
+            />
+          </div>
+          <div class="form-group m-0">
+            <label for="user-confirm-password">Confirm Password</label>
+            <input
+              type="password"
+              class="form-control form-contraol-sm"
+              id="user-confirm-password"
+              name="user-confirm-password"
+              placeholder="Enter Password"
+              required
+            />
+          </div>
+          <div class="form-group m-0 mt-2 d-flex justify-content-center">
+            <button type="submit" name="submit" class="btn">Signup</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
 </div>
 <script>
-document.getElementById("frontpage").style.height = window.innerHeight + "px";
-
+  document.getElementById("frontpage").style.height = window.innerHeight + "px";
 </script>
 <?php require './components/footer.php'?>
